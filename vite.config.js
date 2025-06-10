e675ef7edf26bb1e61e636bf7d6bba1e3266dfb5
Adebayo Temi <temiq3@gmail.com>
2025-06-10 14:43:37 +0100

    updated code

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,47 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa';
-
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Simplifying Progressive Web App (PWA) Development with Vite: A Beginners Guide',
-        short_name: 'PWA Guide',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/imgs/icon512_maskable.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          },
-          {
-            src: '/imgs/icon512_rounded.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          }
-        ]
-      },
-      includeAssets: ['favicon.ico', 'imgs/icon512_maskable.png', 'imgs/icon512_rounded.png'],
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
-      }
-    })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssets:['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Campus Connect",
+    short_name:"Campus connect",
+    description:"I am a simple vite app",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'any'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'any',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
   ],
-});
+  theme_color:'#000000',
+  background_color:'#ffffff',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+})
