d093c4bcde69b2ec9a7fd44a8b8782d6cba15cf2
Adebayo Temi <temiq3@gmail.com>
2025-07-19 22:03:29 +0100

    added a lot of things

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -31,13 +31,13 @@ const BlogPostCard = ({ content, author }) => {
     <h1 className="blog-title">{title}</h1>
     <p className="my-3 text-xl font-gelasio leading-7 max-sm:hidden md:max-[1100px]:hidden line-clamp-2 ">{des}</p>
 
-    <div className="flex gap-4 mt-7">
+    <div className="flex gap-2 mt-7">
       <span className="btn-light py-1 px-4">{tags[0]}</span>
-      <span className="ml-3 flex items-center gap-2 text-dark-grey">
+      <span className="ml-1 flex items-center gap-2 text-dark-grey">
         <i className="fi fi-rr-heart text-xl"></i>
         {total_likes}
       </span>
-      <span className="ml-3 flex items-center gap-2 text-dark-grey">
+      <span className="ml-1 flex items-center gap-2 text-dark-grey">
         <i className="fi fi-rr-comment-dots text-xl"></i>
         {total_comments}
       </span>
