bf696e144de27826c635940f94d8ebc069a60b22
Adebayo Temi <temiq3@gmail.com>
2025-07-21 14:29:55 +0100

    added a lot of things

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -690,13 +690,6 @@ const handleSidebarClose = ()=> {
             <div className='bg-white border-b border-gray-200 p-4'>
               <div className='flex items-center justify-between'>
                 <div className='flex items-center space-x-3'>
-                  {/* Back button for mobile */}
-                  <button
-                    onClick={() => setShowSidebar(true)}
-                    className='p-2 hover:bg-gray-100 rounded-full transition-colors lg:hidden'
-                  >
-                    <ArrowLeft className='w-5 h-5 text-gray-600' />
-                  </button>
 
                   {/* Menu button for mobile when no chat selected */}
                   <button
