23d772b61b47ca4984bdf20af9a7807db65c139c
Adebayo Temi <temiq3@gmail.com>
2025-08-05 13:35:43 +0100

    updated code

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -187,7 +187,7 @@ const Ranking = () => {
               </div>
               <span className="font-semibold">{GetCountDownToMonday()} days left</span>
             </div>
-            <div className="flex items-center gap-3 bg-white/70 backdrop-blur-sm px-4 py-2 rounded-full shadow-md">
+            <div className="hidden md:flex  items-center gap-3 bg-white/70 backdrop-blur-sm px-4 py-2 rounded-full shadow-md">
               <div className="p-2 bg-pink-100 rounded-full">
                 <TrendingUp className="w-5 h-5 text-pink-600" />
               </div>
