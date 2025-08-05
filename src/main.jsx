e8f993748c7e59d9abf069ae93c5e19856b09a9d
Adebayo Temi <temiq3@gmail.com>
2025-08-05 16:22:47 +0100

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop.jsx'
+import { Analytics } from '@vercel/analytics/react';
 
 
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ScrollToTop />
     {/* ScrollToTop component to reset scroll position on route change */}
     <App />
+    <Analytics />
     </BrowserRouter>
   // {/* </React.StrictMode>, */}
 )
