504aa9e224e42417de72697fc847db218928d163
Adebayo Temi <temiq3@gmail.com>
2025-12-11 02:10:17 +0100

    updated code

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -137,13 +137,14 @@ const TweetCard = ({ tweet, author, id }) => {
       });
   };
 
-  const [isFollowing, setIsFollowing] = useState(false);
+    const isFollowing = userAuth?.following?.includes(id);
 
-  useEffect(() => {
-    if (userAuth?.following && id) {
-      setIsFollowing(userAuth.following.includes(id));
-    }
-  }, [userAuth, id]);
+
+  // useEffect(() => {
+  //   if (userAuth?.following && id) {
+  //     setIsFollowing(userAuth.following.includes(id));
+  //   }
+  // }, [userAuth, id]);
 
   const handleFollow = async () => {
     if (!access_token) {
@@ -154,7 +155,7 @@ const TweetCard = ({ tweet, author, id }) => {
     const newState = !isFollowing;
 
     // optimistic UI
-    setIsFollowing(newState);
+    // setIsFollowing(newState);
 
     try {
       const { data } = await axios.post(
@@ -234,7 +235,7 @@ const TweetCard = ({ tweet, author, id }) => {
                     <img
                       src={verifiedBadge}
                       alt='verified'
-                      className='w-4 h-4'
+                      className='w-6 h-6 mb-3'
                     />
                   )}
                 </div>
